5e8e7effbbec02e65a30f4d5833aaf51fe8351e8
incomingstick <incomingstick@gmail.com>
2016-02-29 09:51:17 -0600

    updated header files to match proper conventions

---
+++ prog02/src/prog02lib.h
@@ -0,0 +1,34 @@
+#ifndef _prog02lib
+#define _prog02lib
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class SpellCheck {
+
+public:
+/// <summary>
+/// returns true if the word word is found in the dictionary
+/// file named dict. When word_search is called is must open
+/// the dict file using a local ifstream variable, and it must
+/// close the file before it returns. The function returns
+/// false if the word is not found or if the dictionary file
+/// is not valid.
+/// </summary>
+bool word_search(string word, string dict);
+
+/// <summary>
+/// prints the error message
+/// "word is spelled wrong, on line: line"
+/// to the specified stream. The function returns true if the
+/// message is printed successfully, false if there is a problem
+/// (if, for example, out is an ofstream object not associated
+/// with an open file.)
+/// </summary>
+bool bad_word(string word, int line, ostream& out);
+
+};
+
+#endif
