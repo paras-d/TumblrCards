b0815ca3970f8893c6698dd541a79c3b3553ef1f
incomingstick <incomingstick@gmail.com>
2016-03-07 19:36:11 -0600

    added main function to create program entry point

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int main(int argc, char* argv[]) {
+	cout << "~~Welcome to Tumblr Cards~~" << endl;
+	return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,11 @@
+#ifndef _Main
+#define _Main
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]);
+
+
+#endif
