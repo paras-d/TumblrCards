d8785755eb54c40a1d12a960ab2b07a8864d6917
incomingstick <incomingstick@gmail.com>
2016-05-06 02:24:07 +0100

    Fixed a few typos

--- src/console/console.cpp
+++ src/console/console.cpp
@@ -29,10 +29,10 @@ Console::~Console() {
 bool Console::add_image(ImageMap image) {
     images.push_back(image);
     vector<vector<char>> map = image.get_map();
-    int height = map.size();
-    for(unsigned int y = image.get_y(); y < height); y++) {
+    unsigned int height = map.size();
+    for(unsigned int y = image.get_y(); y < height; y++) {
     	if(y >= get_height() - 1) break;
-    	int width = map[y].size();
+    	unsigned int width = map[y].size();
 		for(unsigned int x = image.get_x(); x < width; x++) {
 			if(x >= get_width()) break;
 			console[y][x] = map[0][0];
